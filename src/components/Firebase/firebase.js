e80aeeb93379877a9ea030aaea43e1629a731299
Nick Lauwers <nicholaslauwers04@gmail.com>
2020-07-16 14:45:42 -0700

    Add real-time database

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -13,7 +14,9 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
     this.auth = app.auth();
+    this.db = app.database();
   }
  
   // *** Auth API ***
@@ -30,6 +33,12 @@ class Firebase {
  
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref('users');
 }
  
 export default Firebase;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -33,8 +32,17 @@ class SignUpFormBase extends Component {
 		this.props.firebase
 			.doCreateUserWithEmailAndPassword(email, passwordOne)
 			.then(authUser => {
-				this.setState({ ...INITIAL_STATE });
-				this.props.history.push(ROUTES.HOME);
+				// Create a user in your Firebase realtime database
+				this.props.firebase
+					.user(authUser.user.uid)
+					.set({ username, email })
+					.then(() => {
+						this.setState({ ...INITIAL_STATE });
+						this.props.history.push(ROUTES.HOME);
+					})
+					.catch(error => {
+						this.setState({ error });
+					});
 			})
 			.catch(error => {
 				this.setState({ error });
@@ -95,7 +103,7 @@ const SignUpLink = () => (
 	</p>
 );
 
-const SignUpForm = compose(withRouter, withFirebase)(SignUpFormBase);
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
 export default SignUpPage;
 
